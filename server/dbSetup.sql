2506461a2e8e25e1c3b767eb9428a95872879eca
Anthony Lee <anthonylee5519@gmail.com>
2025-01-16 13:51:42 -0700

    recipe table

--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -6,3 +6,15 @@ CREATE TABLE IF NOT EXISTS accounts(
   email VARCHAR(255) UNIQUE COMMENT 'User Email',
   picture VARCHAR(255) COMMENT 'User Picture'
 ) default charset utf8mb4 COMMENT '';
+
+CREATE TABLE recipes(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  creator_id VARCHAR(255) NOT NULL,
+  title TINYTEXT NOT NULL,
+  instructions TEXT,
+  img TEXT NOT NULL,
+  category ENUM('breakfast', 'lunch', 'dinner', 'snack', 'dessert') NOT NULL,
+  FOREIGN KEY (creator_id) REFERENCES accounts(id) ON DELETE CASCADE
+)
