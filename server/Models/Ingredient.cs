b974b741faf3c15f669912cbafa382205b7e2548
Anthony Lee <anthonylee5519@gmail.com>
2025-01-16 16:03:10 -0700

    ingredients model

---
+++ server/Models/Ingredient.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace all_spice_dotnet.Models;
+
+public class Ingredient
+{
+  public int Id { get; set; }
+  public DateTime CreatedAt { get; set; }
+  public DateTime UpdatedAt { get; set; }
+  public int RecipeId { get; set; }
+  [MinLength(0), MaxLength(255)] public string Name { get; set; }
+  [Range(0, 255)] public int Quantity { get; set; }
+}
