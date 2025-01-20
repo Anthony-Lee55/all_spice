0f8546beca0dc50a7a62cef50fa90a78d00347e4
Anthony Lee <anthonylee5519@gmail.com>
2025-01-20 15:37:20 -0700

    storing recipes in appstate\

--- client/src/services/RecipesService.js
+++ client/src/services/RecipesService.js
@@ -1,11 +1,15 @@
 import { logger } from "@/utils/Logger.js"
 import { api } from "./AxiosService.js"
+import { Recipe } from "@/models/Recipe.js"
+import { AppState } from "@/AppState.js"
 
 
 class RecipesService{
   async getRecipes() {
     const response = await api.get('api/recipes')
     logger.log("GOT RECIPES", response.data)
+    const recipes = response.data.map(recipePOJO => new Recipe(recipePOJO))
+    AppState.recipes = recipes
   }
 
 }
